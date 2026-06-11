58e223bd25e1016726458220144ed9b4719069b3
Miguel_23300705-cmd <a23300705@ceti.mx>
2026-06-10 18:23:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/clinicfiles/Paciente.kt
@@ -0,0 +1,12 @@
+package com.example.clinicfiles
+
+data class Paciente( val Nombre: String,
+                     val Diagnostico: String,
+                     val Edad: Int,
+                     val TelefonoEmergencia: Int,
+                     val Domicilio: String,
+                     val Sexo:String,
+                     val EstCivil:String,
+                     val Seguro:String,
+                     val Alergias: String,
+                     val FumaToma: String)
---
+++ app/src/main/java/com/example/clinicfiles/PacientesLista.kt
@@ -0,0 +1,5 @@
+package com.example.clinicfiles
+
+object PacientesLista {
+        val listaCafes = mutableListOf<Paciente>()
+    }
